845718979e5debb64a9f7dc38076c3e3dffcc722
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-04 11:44:20 -0300

    Adicionando correção de loops

--- ex-11.py
+++ ex-11.py
@@ -1,2 +1,14 @@
 # 11 - Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. 
 # Um número primo é aquele que é divisível somente por ele mesmo e por 1.
+
+num = 25
+i = 2
+while i < num:
+    print(f"{num}%{i} = {num%i}")
+    if num % i == 0:
+        print(f'{num} NÃO É PRIMO')
+        break
+    elif i >= num/2:
+        print(f"{num} é primo, parabéns")
+        break
+    i+=1
